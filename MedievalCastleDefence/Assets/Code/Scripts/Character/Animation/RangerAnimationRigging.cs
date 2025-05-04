57cc20260771d7dc8abfe01702948bca63113896
Onurcan <onr.k@hotmail.com>
2025-05-04 04:32:29 +0300

    ++

--- MedievalCastleDefence/Assets/Code/Scripts/Character/Animation/RangerAnimationRigging.cs
+++ MedievalCastleDefence/Assets/Code/Scripts/Character/Animation/RangerAnimationRigging.cs
@@ -10,7 +10,12 @@ public class RangerAnimationRigging : CharacterRegistry
     [SerializeField] private MultiAimConstraint _leftHandConstrait;
     [SerializeField] private MultiAimConstraint _rightHandConstrait;
     [Networked(OnChanged = nameof(OnNetworkDrawStateChange))] public NetworkBool IsPlayerDrawing { get; set; }
-    [SerializeField] private NetworkRigidbody _networkRigidbody;
+    [SerializeField] private NetworkRigidbody _networkLeftArmRigidbody;
+    [SerializeField] private NetworkRigidbody _networkHeadRigidbody;
+    [SerializeField] private NetworkRigidbody _networkRightHandRigidbody;
+    [SerializeField] private Transform _leftArm;
+    [SerializeField] private Transform _head;
+    [SerializeField] private Transform _rightHand;
     public override void Spawned()
     {
         if (!Object.HasStateAuthority) return;
@@ -32,7 +37,19 @@ public class RangerAnimationRigging : CharacterRegistry
 
     private static void OnNetworkDrawStateChange(Changed<RangerAnimationRigging> changed)
     {
-        changed.Behaviour._networkRigidbody.enabled = changed.Behaviour.IsPlayerDrawing;
+        if (changed.Behaviour.IsPlayerDrawing)
+        {
+            changed.Behaviour._networkLeftArmRigidbody.InterpolationTarget = changed.Behaviour._leftArm;
+            changed.Behaviour._networkHeadRigidbody.InterpolationTarget = changed.Behaviour._head;
+            changed.Behaviour._networkRightHandRigidbody.InterpolationTarget = changed.Behaviour._rightHand;
+        }
+        else
+        {
+            changed.Behaviour._networkHeadRigidbody.InterpolationTarget = null;
+            changed.Behaviour._networkLeftArmRigidbody.InterpolationTarget = null;
+            changed.Behaviour._networkRightHandRigidbody.InterpolationTarget = null;
+        }
+           
     }
         
 }
